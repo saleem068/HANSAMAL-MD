b035549a89ffd9a6f5c9d7e75366c9c2d451b6d5
saleem068 <110615193+saleem068@users.noreply.github.com>
2024-08-27 19:49:41 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,7 +18,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "IMALKA-HANSAMAL",
+  OWNER_NAME: process.env.OWNER_NAME || "I'm.Mughal",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "923023752640",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
